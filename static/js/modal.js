40e76551adc607ffa0488bbd8dc076163942e003
Alex <alex96ford19@gmail.com>
2020-05-12 19:43:58 +0100

    Added more sections and a django modal

--- static/js/modal.js
+++ static/js/modal.js
@@ -8,26 +8,28 @@ $('.sassModalButton').on('click', function() {
         tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
     } else {
         tl.to(modalTarget, { duration: 0.5, left: 0, top: 800, backgroundColor: 'white', ease: 'power3' });
-        tl.to('.scroll-container', { duration: 0.5, height: '0vh', backgroundColor: 'rgb(179, 179, 179)', ease: 'power3' }, '-=0.2')
+        tl.to('.scroll-container', { duration: 0.5, height: '0vh', backgroundColor: 'white', ease: 'power3' }, '-=0.2')
         tl.to('.t-letter', { duration: 0.5, bottom: 50, ease: 'bouce', stagger: 0.2 }, '-=0.8')
         tl.to('.arrow-button', { duration: 0.8, rotateY: 0, ease: 'back' }, '-=1.1')
     }
     $(modalTarget).toggleClass('closed')
 });
 
-$('.gsapModalButton').on('click', function() {
-    var modalTarget = '#gsapModal';
+
+// django modal
+$('.djangoModalButton').on('click', function() {
+    var modalTarget = '#djangoModal';
     var tl = gsap.timeline();
     if ($(modalTarget).hasClass('closed')) {
         tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(250, 250, 250)', ease: 'power3' })
         tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
-        tl.to('.t-letter', { duration: 0.5, bottom: 0, ease: 'bounce', stagger: 0.25 }, '-=1.8')
-        tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=1.2')
+        tl.to('.d-letter', { duration: 0.5, bottom: 0, ease: 'bounce', stagger: 0.25 }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
     } else {
-        tl.to(modalTarget, { duration: 0.5, left: 0, top: 800, backgroundColor: 'white', ease: 'power3' });
-        tl.to('.scroll-container', { duration: 0.5, height: '0vh', backgroundColor: 'rgb(179, 179, 179)', ease: 'power3' }, '-=0.2')
-        tl.to('.t-letter', { duration: 0.5, bottom: 50, ease: 'bouce', stagger: 0.2 }, '-=0.8')
-        tl.to('.arrow-button', { duration: 0.8, rotateY: 0, ease: 'back' }, '-=1.1')
+        tl.to(modalTarget, { duration: 0.01, left: 0, top: 800, backgroundColor: 'white', ease: 'power3' });
+        tl.to('.scroll-container', { duration: 0.01, height: '0vh', backgroundColor: 'white', ease: 'power3' }, '-=0.2')
+        tl.to('.d-letter', { duration: 0.01, bottom: '100px', ease: 'bouce', stagger: 0.2 }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.01, rotateY: 0, ease: 'back' }, '-=1.1')
     }
     $(modalTarget).toggleClass('closed')
 });
