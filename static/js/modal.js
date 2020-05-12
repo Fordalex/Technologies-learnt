612ea2a71b73c0c9e8f97e6fdc55152c307fd917
Alex <alex96ford19@gmail.com>
2020-05-12 12:27:07 +0100

    Add the illustration section and the forms section

--- static/js/modal.js
+++ static/js/modal.js
@@ -2,13 +2,13 @@ $('.sassModalButton').on('click', function() {
     var modalTarget = '#sassModal';
     var tl = gsap.timeline();
     if ($(modalTarget).hasClass('closed')) {
-        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, ease: 'power3' })
-        tl.to('.scroll-container', { duration: 0.8, height: '87vh', ease: 'bounce' }, '-=0.1')
+        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(250, 250, 250)', ease: 'power3' })
+        tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
         tl.to('.t-letter', { duration: 0.5, bottom: 0, ease: 'bounce', stagger: 0.25 }, '-=0.8')
         tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
     } else {
-        tl.to(modalTarget, { duration: 0.5, left: 0, top: 800, ease: 'power3' });
-        tl.to('.scroll-container', { duration: 0.5, height: '0vh', ease: 'power3' }, '-=0.2')
+        tl.to(modalTarget, { duration: 0.5, left: 0, top: 800, backgroundColor: 'white', ease: 'power3' });
+        tl.to('.scroll-container', { duration: 0.5, height: '0vh', backgroundColor: 'rgb(179, 179, 179)', ease: 'power3' }, '-=0.2')
         tl.to('.t-letter', { duration: 0.5, bottom: 50, ease: 'bouce', stagger: 0.2 }, '-=0.8')
         tl.to('.arrow-button', { duration: 0.8, rotateY: 0, ease: 'back' }, '-=1.1')
     }
@@ -19,13 +19,13 @@ $('.gsapModalButton').on('click', function() {
     var modalTarget = '#gsapModal';
     var tl = gsap.timeline();
     if ($(modalTarget).hasClass('closed')) {
-        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, ease: 'power3' })
-        tl.to('.scroll-container', { duration: 0.8, height: '87vh', ease: 'bounce' }, '-=0.1')
+        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(250, 250, 250)', ease: 'power3' })
+        tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
         tl.to('.t-letter', { duration: 0.5, bottom: 0, ease: 'bounce', stagger: 0.25 }, '-=1.8')
         tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=1.2')
     } else {
-        tl.to(modalTarget, { duration: 0.5, left: 0, top: 800, ease: 'power3' });
-        tl.to('.scroll-container', { duration: 0.5, height: '0vh', ease: 'power3' }, '-=0.2')
+        tl.to(modalTarget, { duration: 0.5, left: 0, top: 800, backgroundColor: 'white', ease: 'power3' });
+        tl.to('.scroll-container', { duration: 0.5, height: '0vh', backgroundColor: 'rgb(179, 179, 179)', ease: 'power3' }, '-=0.2')
         tl.to('.t-letter', { duration: 0.5, bottom: 50, ease: 'bouce', stagger: 0.2 }, '-=0.8')
         tl.to('.arrow-button', { duration: 0.8, rotateY: 0, ease: 'back' }, '-=1.1')
     }
