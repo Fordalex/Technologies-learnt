9d0e75d948de5d67acb0bf57b6e4afd93a92eef4
Alex <alex96ford19@gmail.com>
2020-05-14 23:02:24 +0100

    On load animation prefected and more content added to sections.

--- static/js/modal.js
+++ static/js/modal.js
@@ -22,9 +22,9 @@ $('.djangoModalButton').on('click', function() {
     var modalTarget = '#djangoModal';
     var tl = gsap.timeline();
     if ($(modalTarget).hasClass('closed')) {
-        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(242, 242, 242)', ease: 'power3' })
+        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(40, 40, 40)', ease: 'power3' })
         tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
-        tl.to('.d-letter', { duration: 0.5, opacity: 1, bottom: 0, ease: 'power3', stagger: 0.07 }, '-=0.8')
+        tl.to('.d-letter', { duration: 0.5, opacity: 1, bottom: 0, ease: 'power3', stagger: 0.07, color: 'white' }, '-=0.8')
         tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
     } else {
         tl.to(modalTarget, { duration: 0.3, left: 0, top: '110vh', backgroundColor: 'white', ease: 'power3' });
