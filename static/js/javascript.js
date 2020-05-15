d96b2a53cb02679860c8beeb10efa7d9ecbe268b
Alex <alex96ford19@gmail.com>
2020-05-15 16:43:17 +0100

    Added mobile navigation

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -7,8 +7,11 @@ tl.to('.right-shutter', 0.7, {}, '-=0.7');
 tl.to('.page-container', 0.1, { opacity: 1 }, '-=0.7')
 tl.to('.left-shutter', 1.5, { x: '-60vw' })
 tl.to('.right-shutter', 1.5, { x: '60vw' }, '-=1.5')
-tl.from('.wobble-onload', 1, { transform: 'scale(0)', ease: 'elastic', stagger: 0.1 }, '-=1.2')
-tl.from('.main-title', 1, { opacity: 0, ease: 'power2' }, '-=0.7')
+tl.from('.wobble-onload', 1, { transform: 'scale(0)', ease: 'elastic', stagger: 0.1 }, '-=1.3')
+tl.from('.main-title', 1, { opacity: 0, ease: 'power2' }, '-=0.8')
+tl.from('.mobile-navigation', 0.7, { top: '-55px' }, '-=0.9')
+
+// open all categories for a selected section.
 
 $('#open-all-front').on('click', function() {
     $('#css').collapse('toggle');
@@ -37,4 +40,20 @@ $('#open-all-back').on('click', function() {
 $('.far').on('click', function() {
     $(this).toggleClass('fa-plus-square')
     $(this).toggleClass('fa-minus-square')
+});
+
+$('.toggle-nav').on('click', function() {
+    var tl = gsap.timeline()
+    if ($('.mobile-navigation').hasClass('closed')) {
+        tl.to('.nav-link-container', 0.5, { left: '40vw' });
+        tl.to('.nav-backdrop', 0.05, { right: '60vw' }, '-=0.5');
+        tl.to('.nav-backdrop', 1, { backdropFilter: 'blur(4px)', backgroundColor: 'rgba(0, 0, 0, 0.25)' }, '-=0.5');
+        tl.from('.nav-button', 0.5, { left: '65vw', stagger: 0.05 }, '-=0.9');
+        $('.mobile-navigation').toggleClass('closed');
+    } else {
+        tl.to('.nav-link-container', 0.5, { left: '100vw' });
+        tl.to('.nav-backdrop', 0.2, { right: '100vw', backdropFilter: 'blur(0px)', backgroundColor: 'rgba(0, 0, 0, 0.0)' }, '-=0.5');
+        $('.mobile-navigation').toggleClass('closed');
+    }
+
 });
--- static/js/modal.js
+++ static/js/modal.js
@@ -1,21 +1,4 @@
-// modal
-$('.sassModalButton').on('click', function() {
-    var modalTarget = '#sassModal';
-    var tl = gsap.timeline();
-    if ($(modalTarget).hasClass('closed')) {
-        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(242, 242, 242)', ease: 'power3' })
-        tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
-        tl.to('.t-letter', { duration: 0.5, bottom: 0, ease: 'power3', stagger: 0.07 }, '-=0.8')
-        tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
-    } else {
-        tl.to(modalTarget, { duration: 0.3, left: 0, top: '110vh', backgroundColor: 'white', ease: 'power3' });
-        tl.to('.scroll-container', { duration: 0.01, height: '0vh', backgroundColor: 'white', ease: 'power3' }, '-=0.2')
-        tl.to('.t-letter', { duration: 0.01, bottom: '1000px', ease: 'power3', stagger: 0.2 }, '-=0.8')
-        tl.to('.arrow-button', { duration: 0.01, rotateY: 0, ease: 'power3' }, '-=1.1')
-    }
-    $(modalTarget).toggleClass('closed')
-});
-
+// Technologies
 
 // django modal
 $('.djangoModalButton').on('click', function() {
@@ -34,6 +17,26 @@ $('.djangoModalButton').on('click', function() {
     }
     $(modalTarget).toggleClass('closed')
 });
+
+// stripe
+$('.sassModalButton').on('click', function() {
+    var modalTarget = '#sassModal';
+    var tl = gsap.timeline();
+    if ($(modalTarget).hasClass('closed')) {
+        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(242, 242, 242)', ease: 'power3' })
+        tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
+        tl.to('.t-letter', { duration: 0.5, bottom: 0, ease: 'power3', stagger: 0.07 }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
+    } else {
+        tl.to(modalTarget, { duration: 0.3, left: 0, top: '110vh', backgroundColor: 'white', ease: 'power3' });
+        tl.to('.scroll-container', { duration: 0.01, height: '0vh', backgroundColor: 'white', ease: 'power3' }, '-=0.2')
+        tl.to('.t-letter', { duration: 0.01, bottom: '1000px', ease: 'power3', stagger: 0.2 }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.01, rotateY: 0, ease: 'power3' }, '-=1.1')
+    }
+    $(modalTarget).toggleClass('closed')
+});
+
+// education
 
 // landing page modal
 $('.landingModalButton').on('click', function() {
