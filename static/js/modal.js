40eb4f89cc76907518e077791e7bf88ea0265193
Alex <alex96ford19@gmail.com>
2020-05-22 19:07:44 +0100

    Add dj_database_url to the python modules and created the modal.

--- static/js/modal.js
+++ static/js/modal.js
@@ -21,6 +21,26 @@ $('.djangoModalButton').on('click', function() {
     $(modalTarget).toggleClass('closed')
 });
 
+// dj_database_url
+$('.djdatabaseModalButton').on('click', function() {
+    var modalTarget = '#dj_database_modal';
+    var tl = gsap.timeline();
+    if ($(modalTarget).hasClass('closed')) {
+        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(40, 40, 40)', ease: 'power3' })
+        tl.to('.mobile-navigation', 0.2, { top: '-55px' }, '-=0.2')
+        tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
+        tl.to('.tt-letter', { duration: 0.5, opacity: 1, bottom: 0, ease: 'power3', stagger: 0.07, color: 'white' }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
+    } else {
+        tl.to(modalTarget, { duration: 0.3, left: 0, top: '110vh', backgroundColor: 'white', ease: 'power3' });
+        tl.to('.mobile-navigation', 0.2, { top: '0px' }, '-=0.2');
+        tl.to('.scroll-container', { duration: 0.01, height: '0vh', backgroundColor: 'white', ease: 'power3' }, '-=0.2')
+        tl.to('.tt-letter', { duration: 0.01, opacity: 0, bottom: '1000px', ease: 'power3', stagger: 0.02 }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.01, rotateY: 0, ease: 'power3' }, '-=1.1')
+    }
+    $(modalTarget).toggleClass('closed')
+});
+
 // stripe
 $('.stripeModalButton').on('click', function() {
     var modalTarget = '#stripeModal';
