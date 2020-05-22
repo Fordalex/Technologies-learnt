5a14e5d82c0a371368ba62fab0505d3f10e57b5d
Alex <alex96ford19@gmail.com>
2020-05-22 21:59:15 +0100

    Add more python dependencies and modals.

--- static/js/modal.js
+++ static/js/modal.js
@@ -21,6 +21,66 @@ $('.djangoModalButton').on('click', function() {
     $(modalTarget).toggleClass('closed')
 });
 
+// gunicorn
+$('.gunicornModalButton').on('click', function() {
+    var modalTarget = '#gunicornModal';
+    var tl = gsap.timeline();
+    if ($(modalTarget).hasClass('closed')) {
+        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(40, 40, 40)', ease: 'power3' })
+        tl.to('.mobile-navigation', 0.2, { top: '-55px' }, '-=0.2')
+        tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
+        tl.to('.gun-letter', { duration: 0.5, opacity: 1, bottom: 0, ease: 'power3', stagger: 0.07, color: 'white' }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
+    } else {
+        tl.to(modalTarget, { duration: 0.3, left: 0, top: '110vh', backgroundColor: 'white', ease: 'power3' });
+        tl.to('.mobile-navigation', 0.2, { top: '0px' }, '-=0.2');
+        tl.to('.scroll-container', { duration: 0.01, height: '0vh', backgroundColor: 'white', ease: 'power3' }, '-=0.2')
+        tl.to('.gun-letter', { duration: 0.01, opacity: 0, bottom: '1000px', ease: 'power3', stagger: 0.02 }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.01, rotateY: 0, ease: 'power3' }, '-=1.1')
+    }
+    $(modalTarget).toggleClass('closed')
+});
+
+// pillow
+$('.pillowModalButton').on('click', function() {
+    var modalTarget = '#pillowModal';
+    var tl = gsap.timeline();
+    if ($(modalTarget).hasClass('closed')) {
+        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(40, 40, 40)', ease: 'power3' })
+        tl.to('.mobile-navigation', 0.2, { top: '-55px' }, '-=0.2')
+        tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
+        tl.to('.pillow-letter', { duration: 0.5, opacity: 1, bottom: 0, ease: 'power3', stagger: 0.07, color: 'white' }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
+    } else {
+        tl.to(modalTarget, { duration: 0.3, left: 0, top: '110vh', backgroundColor: 'white', ease: 'power3' });
+        tl.to('.mobile-navigation', 0.2, { top: '0px' }, '-=0.2');
+        tl.to('.scroll-container', { duration: 0.01, height: '0vh', backgroundColor: 'white', ease: 'power3' }, '-=0.2')
+        tl.to('.pillow-letter', { duration: 0.01, opacity: 0, bottom: '1000px', ease: 'power3', stagger: 0.02 }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.01, rotateY: 0, ease: 'power3' }, '-=1.1')
+    }
+    $(modalTarget).toggleClass('closed')
+});
+
+// whitenoise
+$('.whiteNoiseModalButton').on('click', function() {
+    var modalTarget = '#whiteNoiseModal';
+    var tl = gsap.timeline();
+    if ($(modalTarget).hasClass('closed')) {
+        tl.to(modalTarget, { duration: 0.2, left: 0, top: 0, backgroundColor: 'rgb(40, 40, 40)', ease: 'power3' })
+        tl.to('.mobile-navigation', 0.2, { top: '-55px' }, '-=0.2')
+        tl.to('.scroll-container', { duration: 0.8, height: '87vh', backgroundColor: 'white', ease: 'bounce' }, '-=0.1')
+        tl.to('.dd-letter', { duration: 0.5, opacity: 1, bottom: 0, ease: 'power3', stagger: 0.07, color: 'white' }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.8, rotateY: 720, ease: 'back' }, '-=2.2')
+    } else {
+        tl.to(modalTarget, { duration: 0.3, left: 0, top: '110vh', backgroundColor: 'white', ease: 'power3' });
+        tl.to('.mobile-navigation', 0.2, { top: '0px' }, '-=0.2');
+        tl.to('.scroll-container', { duration: 0.01, height: '0vh', backgroundColor: 'white', ease: 'power3' }, '-=0.2')
+        tl.to('.dd-letter', { duration: 0.01, opacity: 0, bottom: '1000px', ease: 'power3', stagger: 0.02 }, '-=0.8')
+        tl.to('.arrow-button', { duration: 0.01, rotateY: 0, ease: 'power3' }, '-=1.1')
+    }
+    $(modalTarget).toggleClass('closed')
+});
+
 // dj_database_url
 $('.djdatabaseModalButton').on('click', function() {
     var modalTarget = '#dj_database_modal';
